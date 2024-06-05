e4adb3ff445dacd3ceb9a7a83f13d08d31707d0f
NFTonTheBlock <NFTonTheBlock@proton.me>
2024-06-06 01:06:32 +0200

    enabled minting fee

--- assembly/Constants.ts
+++ assembly/Constants.ts
@@ -4,7 +4,7 @@ export namespace Constants {
   export const NAME: string = "The Adoration of the Cryptos";
   export const SYMBOL: string = "Triumph";
   export const MINT_PRICE: u64 = 68515000000;
-  export const MINT_FEE: bool = false;
+  export const MINT_FEE: bool = true;
   export const MAX_SUPPLY: u64 = 3;
   export const URI: string = "https://nftstorage.link/ipfs/bafybeibdocdd4ura4denytmtnuzf2nu4s2zufhfryxtkvrr4nzlroqsply";
   export const OWNER: Uint8Array = Base58.decode("1FRhAeSs1Cci3BPGrMjnHjhKjBVhMSx82v");
